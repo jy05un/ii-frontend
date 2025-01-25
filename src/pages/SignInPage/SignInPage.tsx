47e971f120ad25c317be6cc55ae8a4893ac2470b
taeboranger <lightstone1@naver.com>
2025-01-25 23:27:50 +0900

    [FIX] sign in page styling fixed

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,17 +1,51 @@
 import SignInCard from 'components/SignInCard';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import styles from './SignInPage.module.css';
+import classNames from 'classnames';
 
 export default function SignInPage() {
   const navigate = useNavigate()
+  const [index, setIndex] = useState(0)
+  const [isActive, setIsActive] = useState(true)
+
+  const changeHeaderTopBg = () => {
+    setIsActive(false)
+    setTimeout(() => {
+      setIndex((index + 1) % 4)
+      setIsActive(true)
+    }, 1000)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(changeHeaderTopBg, 5000)
+    return () => clearInterval(interval)
+  })
+
   return (
     <div className={styles.signInPage}>
       <div className={styles.header}>
-        <h1> header </h1>
+        <div className={
+          classNames(styles.headerTop,
+            {
+              [styles.headerTopX]: index == 0
+            },
+            {
+              [styles.headerTopIg]: index == 1
+            },
+            {
+              [styles.headerTopCafe]: index == 2
+            },
+            {
+              [styles.headerTopSoop]: index == 3
+            },
+            {
+              [styles.headerTopActive]: isActive
+            })
+        }></div>
       </div>
       <div className={styles.wrapper}>
-        <SignInCard className={styles.signInCard} onConfirm={() => navigate("/feeds")}/>
+        <SignInCard className={styles.signInCard} onConfirm={() => navigate("/feeds")} />
         <div className={styles.footer}>
           <p> footer </p>
         </div>
